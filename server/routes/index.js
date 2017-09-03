7cb5c4814aff741558361e314c2b39f0cb80cf06
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-09-03 21:06:00 +0200

    saving landscapes

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,6 @@ var path = require('path');
 module.exports = function(app) {
   app.use('/user', require('../api/user'));
   app.use('/api', require('../api/landscape'));
-  // app.use('/comment', require('../api/comment'));
-  // app.use('/messages', require('../api/message'));
-  // app.use('/relations', require('../api/project-user-rel'));
+  app.use('/api', require('../api/map'));
+  app.use('/api', require('../api/favourites'));
 };
