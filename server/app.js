a2ea016e7686505399e5d636b540f03d8f7b7d13
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-09-10 20:47:35 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -72,7 +72,7 @@ app.use((req, res, next)=>{
 require('./routes')(app);
 // catch 404 and forward to error handler
 app.all('/*', function(req, res) {
-  res.sendfile('./index.html');
+  res.sendfile(__dirname + '/public/index.html');
 });
 app.use((req, res, next) => {
   const err = new Error('Not Found');
